2dcb3a1b0487cf4505734a65b8a4194a36207a9b
Lailson Lira <lailsonlira10@gmail.com>
2025-02-26 14:44:07 -0300

    1.6js

---
+++ estilo/scripts.js
@@ -0,0 +1,27 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicators = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+
+let active = 0
+let fristPosition = 0
+let lastPosition = items.length -1
+
+
+nextButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    items[active].classList.add('active')
+}
+
+prevButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active - 1 < fristPositio ? lastPosition : active -1
+    items[active].classList.add('active')
+}
